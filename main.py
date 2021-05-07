d25e3833c902e0d4bc9c712c2fe149754075e1a9
Dan0606 <dandan060605@gmail.com>
2021-05-07 14:10:06 +0300

    added speech reco

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from time import sleep
+import speech_recognition as sr
 
 morse_rep = {"a":[1, 2], "b": [2,1,1,1], "c":[2,1,2,1], "d": [2,1,1], "e": [1], "f": [1,1,2,1], "g": [2,2,1],
             "h":[1,1,1,1], "i": [1,1], "j": [1,2,2,2], "k": [2, 1, 2], "l": [1, 2, 1,1],
@@ -6,8 +7,16 @@ morse_rep = {"a":[1, 2], "b": [2,1,1,1], "c":[2,1,2,1], "d": [2,1,1], "e": [1], 
             "s": [1,1,1], "t": [2], "u": [1,1,2], "v":[1, 1, 1, 2], "w": [1,2,2], "x":[2, 1,1,2], "y":[2, 1, 2, 2],
             "z":[2, 2, 1, 1]}
 
-word = input("Enter A Word: ").lower()
+recognizer = sr.Recognizer()
+with sr.Microphone() as source:
+    print("say something: ")
+    audio = recognizer.listen(source)
+    try:
+        word = recognizer.recognize_google(audio)
+    except:
+        print("sorry, I didn't understand")
 
+print(word)
 sep_word = list(word)
 
 for i in sep_word:
